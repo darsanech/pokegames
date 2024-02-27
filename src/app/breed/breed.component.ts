dab4ce2c69a68744b5a30668fdaf04cf012bdae5
Darío Sánchez Echenique <dsanchez@iconsl.com>
2024-02-27 17:41:47 +0100

    zonas e imagenes movibles

--- src/app/breed/breed.component.ts
+++ src/app/breed/breed.component.ts
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './breed.component.css'
 })
 export class BreedComponent {
+  allowDrop(event: any) {
+    event.preventDefault();
+  }
 
+  drag(event: any) {
+    event.dataTransfer.setData('text', event.target.id);
+  }
+
+  drop(event: any, zonaId: string) {
+    event.preventDefault();
+    const data = event.dataTransfer.getData('text');
+    const draggableElement = document.getElementById(data);
+    const contenedor = document.getElementById(zonaId);
+    if(draggableElement!=null && contenedor!=null){
+      contenedor.appendChild(draggableElement);
+      const offsetX = event.clientX - contenedor.getBoundingClientRect().left - draggableElement.clientWidth / 2;
+      const offsetY = event.clientY - contenedor.getBoundingClientRect().top - draggableElement.clientHeight / 2;
+      draggableElement.style.left = offsetX + 'px';
+      draggableElement.style.top = offsetY + 'px';
+    }
+  }
 }
