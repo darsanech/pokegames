2d12cfa37d9ad722523da79751f1735d6051a058
Darío Sánchez Echenique <dsanchez@iconsl.com>
2024-02-28 17:59:10 +0100

    ViewChild y Renderer descubiertos

--- src/app/breed/breed.component.ts
+++ src/app/breed/breed.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild, viewChildren } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { BreedserviceService } from '../services/breedservice.service';
 import { CommonModule } from '@angular/common';
 import { PatronMovimientos, Pokemon } from '../pokemons';
@@ -21,7 +21,7 @@ export class BreedComponent implements OnInit{
   @ViewChild('zona3') zona3!: ElementRef;
   @ViewChild('zona4') zona4!: ElementRef;
 
-  constructor(private breedservice: BreedserviceService){
+  constructor(private breedservice: BreedserviceService, private renderer:Renderer2){
     this.pokemons=breedservice.guarderia;
   }
   ngOnInit(): void {
@@ -29,22 +29,19 @@ export class BreedComponent implements OnInit{
 
   }
   randomNumber(){ 
-    const contenedor = document.getElementById('zona0');
-    if(contenedor!=null){
       for(let i=0; i<=10; i++){
         this.randomoffset[i]={
-            posx:Math.floor(Math.random() * (contenedor.getBoundingClientRect().width - 150)),
-            posy:Math.floor(Math.random() * (contenedor.getBoundingClientRect().height - 150)),
+            posx:Math.floor(Math.random() * (this.zona0.nativeElement.width - 150)),
+            posy:Math.floor(Math.random() * (this.zona0.nativeElement.height - 150)),
             delay:i/4+"s"
         }
       }
-    }
   }
 
   //comprobar quienes estan en cada zona
   pasarLista(){
     for(let j=0; j<this.zona0.nativeElement.children.length; j++){
-      console.log(this.zona0.nativeElement.children[j].id)
+      console.log(this["zona0"].nativeElement.children[j].id)
     }
 
   }
@@ -60,12 +57,10 @@ export class BreedComponent implements OnInit{
     event.preventDefault();
     const data = event.dataTransfer.getData('text');
     const draggableElement = document.getElementById(data);
-    const contenedor = document.getElementById(zonaId);
-
-    if(draggableElement!=null && contenedor!=null){
-      contenedor.appendChild(draggableElement);
-      const offsetX = event.clientX - contenedor.getBoundingClientRect().left - draggableElement.clientWidth / 2;
-      const offsetY = event.clientY - contenedor.getBoundingClientRect().top - draggableElement.clientHeight / 2 ;
+    if(draggableElement!=null){
+      this.renderer.appendChild(this[zonaId].nativeElement,draggableElement)
+      const offsetX = event.clientX - this[zonaId].nativeElement.offsetLeft - draggableElement.clientWidth / 2;
+      const offsetY = event.clientY - this[zonaId].nativeElement.offsetTop - draggableElement.clientHeight / 2 ;
       draggableElement.style.left = offsetX + 'px';
       draggableElement.style.top = offsetY + 'px';
     }
