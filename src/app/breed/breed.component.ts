984ff8317c3c281551daf310562b3db4d2f6d6a0
Darío Sánchez Echenique <dsanchez@iconsl.com>
2024-02-28 17:36:42 +0100

    Pasar lista de cada guarderia

--- src/app/breed/breed.component.ts
+++ src/app/breed/breed.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, viewChildren } from '@angular/core';
 import { BreedserviceService } from '../services/breedservice.service';
 import { CommonModule } from '@angular/common';
 import { PatronMovimientos, Pokemon } from '../pokemons';
@@ -15,6 +15,11 @@ export class BreedComponent implements OnInit{
   [x: string]: any;
   pokemons:Array<Pokemon>;
   randomoffset:Array<PatronMovimientos>=new Array<PatronMovimientos>(10);
+  @ViewChild('zona0') zona0!: ElementRef;
+  @ViewChild('zona1') zona1!: ElementRef;
+  @ViewChild('zona2') zona2!: ElementRef;
+  @ViewChild('zona3') zona3!: ElementRef;
+  @ViewChild('zona4') zona4!: ElementRef;
 
   constructor(private breedservice: BreedserviceService){
     this.pokemons=breedservice.guarderia;
@@ -24,7 +29,7 @@ export class BreedComponent implements OnInit{
 
   }
   randomNumber(){ 
-    const contenedor = document.getElementById('zona1');
+    const contenedor = document.getElementById('zona0');
     if(contenedor!=null){
       for(let i=0; i<=10; i++){
         this.randomoffset[i]={
@@ -32,9 +37,16 @@ export class BreedComponent implements OnInit{
             posy:Math.floor(Math.random() * (contenedor.getBoundingClientRect().height - 150)),
             delay:i/4+"s"
         }
-        console.log(this.randomoffset[i].delay)
       }
     }
+  }
+
+  //comprobar quienes estan en cada zona
+  pasarLista(){
+    for(let j=0; j<this.zona0.nativeElement.children.length; j++){
+      console.log(this.zona0.nativeElement.children[j].id)
+    }
+
   }
 
   //movimiento
